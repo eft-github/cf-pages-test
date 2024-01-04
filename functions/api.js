cd18489142e5f5a8a3752eb0287e8141927cd3f5
eft-github <142958730+eft-github@users.noreply.github.com>
2024-01-04 15:29:47 +0800

    onRequestGet again

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export async function onRequest(context) {
+export async function onRequestGet(context) {
     try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
         return new Response(JSON.stringify(response.data), {
